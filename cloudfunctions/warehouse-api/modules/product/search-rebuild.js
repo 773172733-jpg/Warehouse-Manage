cccb7a7235a2dead51e7d0cb5eca80096d2b1047
773172733-jpg <773172733@qq.com>
2026-07-18 13:09:09 +0800

    fix: resolve trusted user for search rebuild

--- cloudfunctions/warehouse-api/modules/product/search-rebuild.js
+++ cloudfunctions/warehouse-api/modules/product/search-rebuild.js
@@ -1,7 +1,9 @@
+const { requireUser } = require('../../common/auth.js');
 const { getDatabase } = require('../../common/database.js');
 const { rebuildProductSearch } = require('./product-service.js');
 
 module.exports = async function handleProductSearchRebuild({ data, context, cloud }) {
   const db = getDatabase(cloud);
-  return rebuildProductSearch(db, context.user, data);
+  const user = await requireUser(db, context);
+  return rebuildProductSearch(db, user, data);
 };
